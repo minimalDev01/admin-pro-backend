e1e2c5f43c785c29beba26fd65a87d2eca2a47d1
Sergio Carralero <sergiocn.dev@gmail.com>
2020-12-14 14:37:23 +0100

    SPA control

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -27,6 +28,11 @@ app.use("/api/hospitals", require("./routes/hospitals"));
 app.use("/api/medics", require("./routes/medics"));
 app.use("/api/all", require("./routes/search"));
 app.use("/api/upload", require("./routes/uploads"));
+
+// The last
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
